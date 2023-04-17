91f93d662dd337b2f19ed12bda67c16efb148a99
unknown <tuan.lenguyen@di4l.vn>
2023-04-17 11:13:05 +0700

    ??

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
 
+// $oaCallbackUrl = "https://www.callbackPage.com"
+// $codeChallenge = "your code challenge";
+// $state = "your state";
+// $linkOAGrantPermission2App = $helper->getLoginUrlByOA($oaCallbackUrl, $codeChallenge, $state);
 echo "2";
